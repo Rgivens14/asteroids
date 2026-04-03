7a7e8ccd8c7d7450cc308c44ce4edc67162ffee2
Rgivens14 <rgivens14@gmail.com>
2026-04-03 12:09:47 -0400

    Complete creation of ship and movement

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,6 +2,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,6 +13,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    
 
     while True:
         log_state()
@@ -19,9 +26,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        
+        updatable.update(dt)
 
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
 
