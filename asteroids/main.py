09b3829efb63ca8629614ef494a2fefeb8ef63c8
Rgivens14 <rgivens14@gmail.com>
2026-04-02 21:30:04 -0400

    Completed setup of screen and dt

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,3 +1,4 @@
+
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
@@ -9,15 +10,20 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        
 
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
